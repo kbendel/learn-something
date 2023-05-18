f6527f62f53aee948d1b96dd22617ba661282dc4
Kevin <kevinbendel@hotmail.com>
2023-05-18 21:23:54 +0000

    Testing API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import logo from './logo.jpg';
 import './App.css';
 import Question from './Question.js'
 import Answer from './Answer.js'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://q2ajp06ktk.execute-api.us-east-2.amazonaws.com/default/learnSomething');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  fetchData();
+}, []);
+  
+  
   return (
     <div class="container">
       <div class="row">
         <div class="col-12">
           <h1 class="text-center">Learn Something</h1>
+          {data}
         </div>
       </div>
       <div class="row">
